1691038e7471c950eb3fe50dfc11e04d6c63fec3
Kim <32936089+kim222@users.noreply.github.com>
2025-10-29 17:06:25 +0000

    fix: deprecation warning
    body-parser deprecated undefined extended: provide extended option app.js:42:17
    
    Need to say true or false and true is preferred to allow for nested objects etc

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ app.use("/api", apiRouter);
 app.use("/", indexRouter);
 
 app.use(express.json({ limit: "100mb" }));
-app.use(express.urlencoded({ limit: "100mb" }));
+app.use(express.urlencoded({ limit: "100mb", extended: true }));
 app.use(express.urlencoded({ extended: false }));
 
 //throw 404 if URL not found
